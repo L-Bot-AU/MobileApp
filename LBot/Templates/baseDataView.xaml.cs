06e7a84a720a5c7211900926956cbcfbac8a1786
JamesTreloar <54793877+JamesTreloar@users.noreply.github.com>
2021-07-12 19:53:14 +1000

    Added other potential home view

--- LBot/Templates/baseDataView.xaml.cs
+++ LBot/Templates/baseDataView.xaml.cs
@@ -12,15 +12,44 @@ using SkiaSharp;
 namespace LBot.Templates {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class baseDataView:ContentView {
+
+
+        public ChartEntry[] getGraphData(string lib) {
+
+            int[] nums;
+            if (lib=="jnr") {
+                nums = new int[] { 70, 40, 90 };
+            } else {
+                nums = new int[] { 40, 50, 100 };
+            }
+
+            ChartEntry[] entries = new[] {
+                new ChartEntry(nums[0]) {
+                    Label = "Before School",
+                    ValueLabel = nums[0].ToString(),
+                },
+                new ChartEntry(nums[1]) {
+                    Label = "Break 1",
+                    ValueLabel = nums[1].ToString(),
+                },
+                new ChartEntry(nums[2]) {
+                    Label = "Break 2",
+                    ValueLabel = nums[2].ToString(),
+                },
+            };
+            return entries;
+        }
+
+
         public baseDataView() {
             InitializeComponent();
-
             App.libBase lib = (Application.Current as App).libInfo;
             App.pageBase page = (Application.Current as App).pageInfo;
+            string libraryName = lib.currentLibrary;
 
             if (lib.currentLibrary =="jnr") {
                 LibraryTitle.Text="Junior Library";
-                AlertText.Text="Junior Library Closed for stocktake";
+                AlertText.Text="Junior library closed for stocktake";
             } else {
                 LibraryTitle.Text="Senior Library";
                 AlertText.Text="Year 7s detected in the senior library";
@@ -31,25 +60,11 @@ namespace LBot.Templates {
                 FuturePredictions.IsVisible = false;
             }
 
-
-
-            var entries = new[] {
-                new ChartEntry(70) {
-                    Label = "Before School",
-                     ValueLabel = "70",
-                },
-                new ChartEntry(40) {
-                    Label = "Period 1",
-                    ValueLabel = "40",
-                },
-                new ChartEntry(90) {
-                    Label = "Period 2",
-                    ValueLabel = "90",
-                
-                },
-            };
+           
+            ChartEntry[] entries =  getGraphData(libraryName);
             BarChart chart = new BarChart { Entries = entries, ValueLabelOrientation=Orientation.Horizontal, MaxValue=110, LabelOrientation=Orientation.Horizontal};
             Chart.Chart = chart;
         }
+        
     }
 }
---
+++ LBot/Views/Home1.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LBot.Views {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Home1:ContentPage {
+        public Home1() {
+            (Application.Current as App).pageInfo.currentPage = "home1";
+            InitializeComponent();
+        }
+    }
+}

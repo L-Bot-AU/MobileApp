101e08bcedc859d1bab407c85ec1ab53b30fa580
JamesTreloar <54793877+JamesTreloar@users.noreply.github.com>
2021-07-15 21:53:20 +1000

    Adjusted Colours

--- LBot/AppShell.xaml.cs
+++ LBot/AppShell.xaml.cs
@@ -1,9 +1,20 @@
 ﻿using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace LBot {
     public partial class AppShell:Shell {
         public AppShell() {
             InitializeComponent();
+        }
+
+        public void homePageChoice(int home_page_selection) {
+            if(home_page_selection == 0) {
+                Home0.IsVisible=true;
+                Home1.IsVisible=false;
+            } else {
+                Home0.IsVisible=false;
+                Home1.IsVisible=true;
+            }
         }
     }
 }
